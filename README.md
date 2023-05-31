91b04b87d800b4ec4feff6977837cbf0489bac53
Bastian Wegge <wegge@crossbow.de>
2023-05-31 16:55:30 +0200

    Initialize - Bug? AntdInferencer is ignoring dataProviderName set in resources

---
+++ README.md
@@ -0,0 +1,3 @@
+### refine reproduction for duplicate resource name definitions
+
+This is an example produced with `npm create refine-app@latest -- --example data-provider-multiple` to show the effect of multiple resources with the same name.
---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import {
+    notificationProvider,
+    ThemedLayoutV2,
+    ErrorComponent,
+    RefineThemes,
+} from "@refinedev/antd";
+import dataProvider from "@refinedev/simple-rest";
+import routerProvider, {
+    NavigateToResource,
+    UnsavedChangesNotifier,
+    DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
+import { ConfigProvider } from "antd";
+import "@refinedev/antd/dist/reset.css";
+import { AntdInferencer } from '@refinedev/inferencer/antd';
+
+const API_URL = "https://api.fake-rest.refine.dev";
+const CATEGORIES_API_URL = "https://api.fake-rest.refine.dev";
+const TYPICODE_API_URL = "https://jsonplaceholder.typicode.com";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <ConfigProvider theme={RefineThemes.Blue}>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={{
+                        default: dataProvider(API_URL),
+                        categories: dataProvider(CATEGORIES_API_URL),
+                        typicode: dataProvider(TYPICODE_API_URL),
+                    }}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: "/default/posts",
+                            meta: {
+                                parent: "default",
+                                dataProviderName: "default",
+                            },
+                        },
+                        {
+                            name: "categories",
+                            meta: {
+                                dataProviderName: "categories",
+                            },
+                        },
+                        {
+                            name: "posts",
+                            list: "/typicode/posts",
+                            meta: {
+                                parent: "typicode",
+                                dataProviderName: "typicode",
+                            },
+                        },
+                    ]}
+                    notificationProvider={notificationProvider}
+                    options={{
+                        syncWithLocation: true,
+                        warnWhenUnsavedChanges: true,
+                    }}
+                >
+                    <Routes>
+                        <Route
+                            element={
+                                <ThemedLayoutV2>
+                                    <Outlet />
+                                </ThemedLayoutV2>
+                            }
+                        >
+                            <Route
+                                index
+                                element={
+                                    <NavigateToResource resource="posts" />
+                                }
+                            />
+                            <Route path="/default/posts" element={<AntdInferencer />} />
+                            <Route path="/typicode/posts" element={<AntdInferencer />} />
+                            <Route path="*" element={<ErrorComponent />} />
+                        </Route>
+                    </Routes>
+                    <UnsavedChangesNotifier />
+                    <DocumentTitleHandler />
+                </Refine>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+};
+
+export default App;
